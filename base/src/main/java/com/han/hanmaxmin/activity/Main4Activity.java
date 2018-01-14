195f40a08bb77e8b61fa6ddf3cb0157fa56c56a6
yinzh <yinzh@grapedu.cn>
2018-01-14 19:48:16 +0800

    HanMaxMin Http初始化。《》

---
+++ base/src/main/java/com/han/hanmaxmin/activity/Main4Activity.java
@@ -0,0 +1,14 @@
+package com.han.hanmaxmin.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.han.hanmaxmin.R;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
