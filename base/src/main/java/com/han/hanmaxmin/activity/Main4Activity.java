61f42681804426b7268e9581359d41f5b16c510b
yinzh@grapedu.cn <yinzh@grapedu.cn>
2018-02-09 18:06:41 +0800

    AS 3.0 按钮二次单击禁止。接口。

--- base/src/main/java/com/han/hanmaxmin/activity/Main4Activity.java
+++ base/src/main/java/com/han/hanmaxmin/activity/Main4Activity.java
@@ -2,9 +2,12 @@ package com.han.hanmaxmin.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.han.hanmaxmin.R;
+import com.han.hanmaxmin.common.HanNoDoubleClickListener;
+import com.han.hanmaxmin.common.log.L;
 import com.han.hanmaxmin.common.utils.HanHelper;
 import com.han.hanmaxmin.common.utils.PresenterUtils;
 import com.han.hanmaxmin.common.widget.toast.HanToast;
@@ -14,6 +17,7 @@ import com.han.hanmaxmin.mvp.HanABActivity;
 public class Main4Activity extends HanABActivity {
 
     private TextView textView;
+    private TextView textView1;
 
     @Override
     public int layoutId() {
@@ -27,6 +31,22 @@ public class Main4Activity extends HanABActivity {
 
     @Override
     public void initData(Bundle savedInstanceState) {
+        textView = (TextView) findViewById(R.id.textText);
+        textView1 = (TextView) findViewById(R.id.textText1);
+        textView.setOnClickListener(new HanNoDoubleClickListener() {
+            @Override
+            protected void onNoDoubleClick(View v) {
+                L.i("onClick","223456765412345678-------------");
+            }
+        });
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                L.i("onClick","你好你我诶见ISF四很烦ID合法is覅的说法 -------------");
+
+            }
+        });
         HanHelper.getInstance().getHttpHelper().create(AuthApi.class);
 
     }
---
+++ base/src/main/java/com/han/hanmaxmin/common/HanNoDoubleClickListener.java
@@ -0,0 +1,26 @@
+package com.han.hanmaxmin.common;
+
+import android.view.View;
+
+import java.util.Calendar;
+
+/**
+ * Created by ptxy on 2018/2/9.
+ * 解决按钮二次点击问题。
+ */
+
+public abstract class HanNoDoubleClickListener implements View.OnClickListener {
+
+    private static final int MIN_CLICK_DELAY_TIME = 1000;//间隔时间
+    private long lastClickTime = 0;
+    @Override
+    public void onClick(View v) {
+     long currentTime = Calendar.getInstance().getTimeInMillis();
+     if(currentTime - lastClickTime > MIN_CLICK_DELAY_TIME){
+     lastClickTime = currentTime;
+        onNoDoubleClick(v);
+            }
+    }
+
+     protected abstract void onNoDoubleClick(View v);
+}
